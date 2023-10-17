b66b8d3568ab2c4e052610e9c27cb1a33e5b8406
Tyler Bamberg <7506119+tbamberg@users.noreply.github.com>
2023-10-17 15:19:40 -0700

    Add function to randomly determine computer choice

--- RPS.js
+++ RPS.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+// create options array with ‘Rock’, ‘Paper’, ‘Scissors’
+// create a function to get computer's choice
+//// create variable computerChoice and init with a string
+//// choose a random number 0-2 
+//// convert number to choice and store it in computerChoice
+//// return computerChoice
+
+let options = ['Rock', 'Paper', 'Scissors'];
+
+function getComputerChoice() {
+    let computerChoice = "";
+    computerChoice = options[Math.floor(Math.random() * 3)];
+    return computerChoice;
+}
+
+console.log(getComputerChoice());
