8a2d161e65c5e01122230e60a3702a34336d4b14
Tyler Bamberg <7506119+tbamberg@users.noreply.github.com>
2023-10-17 15:39:08 -0700

    Add function to get player choice and check valid

--- RPS.js
+++ RPS.js
@@ -1,16 +1,33 @@
-// create options array with ‘Rock’, ‘Paper’, ‘Scissors’
+let options = ['rock', 'paper', 'scissors'];
+
 // create a function to get computer's choice
-//// create variable computerChoice and init with a string
+//// create variable computerChoice
 //// choose a random number 0-2 
 //// convert number to choice and store it in computerChoice
 //// return computerChoice
 
-let options = ['Rock', 'Paper', 'Scissors'];
-
 function getComputerChoice() {
-    let computerChoice = "";
-    computerChoice = options[Math.floor(Math.random() * 3)];
+    let computerChoice = options[Math.floor(Math.random() * 3)];
     return computerChoice;
 }
 
 console.log(getComputerChoice());
+
+// create a function to get player's choice
+//// prompt player for input and store it in playerChoice
+//// convert playerChoice to lowercase
+//// check if playerChoice is included in options
+////// if true, return playerChoice
+////// if false, call invalidChoice function
+
+function getPlayerChoice() {
+    let playerChoice = prompt("Choose your weapon:");
+    playerChoice = playerChoice.toLowerCase();
+    if (options.includes(playerChoice)) {
+        return playerChoice;
+    } else {
+        invalidChoice();
+    }
+}
+
+console.log(getPlayerChoice());
